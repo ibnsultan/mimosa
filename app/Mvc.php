f61577f7fd251a4fcec64cd23edb62f340f9d3dc
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-04 12:29:45 +0300

    🔮 Add: render function

--- app/Mvc.php
+++ app/Mvc.php
@@ -24,10 +24,32 @@ class MVC{
         $this->response = new Response();
     }
 
+    public function render($view, $data = [])
+    {
+        $blade = new Blade('app/Views', 'app/Cache');
+        $blade->addExtension('blade.jsx', 'blade');
+
+        // check if the view is a directory
+        $newDir = getcwd() .'/app/Views/'. str_replace('.', '/', $view);
+        
+        if (is_dir($newDir)) { $view = $view . '@Main'; }
+
+        if (strpos($view, '@') !== false) :
+            $components = explode('@', $view);
+            $view = str_replace('@', '.', $view); 
+            $data['screenComponents'] = "$components[0].Components";
+        endif;
+
+        exit($blade->make($view, $data));
+    }
+
 
     public function view($view, $data = [])
     {
         $blade = new Blade('app/Views', 'app/Cache');
+
+        // add support for jsx files
+        $blade->addExtension('blade.jsx', 'blade');
         exit($blade->make($view, $data)); 
     }
 
