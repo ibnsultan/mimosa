9a46f73493b777c1bbbb236b42d84ae2dbc6c1f2
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-05 14:40:35 +0300

    🌿 Welcome Page

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,20 +3,15 @@
 namespace App\Controllers;
 
 use App\Mvc;
-use stdClass;
 
 class HomeController extends Mvc
 {
     public function index()
     {
-        $description = new stdClass;
-
-        $description->firstPoint = 'Mimosa is a php framework designed to directly work with react without completely separating the layers of the application';
-        $description->secondPoint = 'It is built on top of Framework-X, therefore speed is guaranteed';
-
+       
         $data = [
-            'title' => 'Welcome to Mimosa!',
-            'descriptions' => $description                
+            'title' => 'Mimosa',
+            'description' => 'This is a framework designed to seamlessly integrate PHP and ReactJS without the need for complete separation between frontend and backend layers.'            
         ];
 
         return $this->render('Home', $data);
