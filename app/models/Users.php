63373cd00a4ddf4faa0a3eb52b714df9475bf951
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-07 01:59:56 +0300

    ✨ New Model structure, includes migration

--- app/models/Users.php
+++ app/models/Users.php
@@ -1,10 +1,29 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Database\Schema\Blueprint;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Users extends Model
 {
-
     protected $table = 'users';
+
+    public function __construct()
+    {
+        $this->createTableIfNotExists();
+    }
+
+    public function createTableIfNotExists()
+    {
+        if (!Capsule::schema()->hasTable($this->table)) {
+            Capsule::schema()->create($this->table, function (Blueprint $table) {
+                
+                // user table structure
+
+            });
+        }
+    }
+
 }
