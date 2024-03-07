1bd0255155abfec087a77b17805d445538143d59
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-07 19:07:30 +0300

    ✨ Global Console Helpers

--- app/console/Helpers.php
+++ app/console/Helpers.php
@@ -4,6 +4,42 @@ namespace App\Console;
 
 class Helpers
 {
+
+    public $color_red = "\033[31m";
+    public $color_blue = "\033[34m";
+    public $color_green = "\033[32m";
+    public $color_yellow = "\033[33m";
+    public $color_reset = "\033[0m";
+
+    protected $value;
+    protected $option;
+    protected $command;
+
+    public function __construct()
+    {        
+        $this->write('');
+        $this->parseArguments();
+    }
+
+    
+    protected function parseArguments() :void
+    {
+        
+        $arguments = array_slice($_SERVER['argv'], 1);
+
+        $this->value = $arguments[1] ?? null;
+
+        if (count($arguments) >= 1):
+            $arguments = explode(':', $arguments[0]);
+
+            $this->command = $arguments[0];
+            $this->option = $arguments[1] ?? null;
+        
+            else: exit( (new \App\Console\Engine)->showHelp() );
+            
+        endif;
+    }
+
     function write() :void {
         
         $args = func_get_args();
