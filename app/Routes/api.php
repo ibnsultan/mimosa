4c9d607a6f7ba13fc0b7dcd6f558990bab75bd78
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-10 01:35:49 +0300

    ✨ Added API authentication service

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 app->group('/api', function() {
-    
+
     app->get('', function() {
         response()->json(['message' => 'Welcome to Mimosa API']);
     });
+
+    app->post('/auth/login', fn()=>(new \App\Controllers\AuthController)->login(true));
 
 });
