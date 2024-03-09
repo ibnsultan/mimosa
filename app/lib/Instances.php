6476ab090c4ac9d90a76a43b9e045160f8a02abc
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-10 01:34:55 +0300

    ✨ Global instances initialization file

--- app/lib/Instances.php
+++ app/lib/Instances.php
@@ -1,3 +1,35 @@
 <?php
 
+/**
+ *------------------------------------------------------------------------------
+ * Instances
+ *------------------------------------------------------------------------------
+ *
+ * Path: app/lib/Instances.php
+ * Author: Abdulbasit Rubeiyya
+ * Last Updated: 8 March 2024
+ *
+ * This file is used to define global instances that can be used throughout the
+ * application. This file is loaded in the app/lib/Functions.php file.
+ *
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Initialize the Faker Factory
+|--------------------------------------------------------------------------
+|
+| This is used to generate fake data for the application.
+|
+*/
 define('faker', (Faker\Factory::create()) ?? null);
+
+/*
+|--------------------------------------------------------------------------
+| Get the Configurations
+|--------------------------------------------------------------------------
+|
+| This is used to get the configurations for the application.
+|
+*/
+define('config', require_once getcwd() . '/config/app.php');
