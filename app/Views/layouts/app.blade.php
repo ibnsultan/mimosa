978bf312928f2f13f97ef21cbb5f8a17964e34fb
ibnsultan <abdulbasitsultan4@gmail.com>
2024-03-10 01:38:27 +0300

    💫 Autoloading scripts and stylesheets in the screen's scope

--- app/Views/layouts/app.blade.php
+++ app/Views/layouts/app.blade.php
@@ -2,11 +2,18 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{env('APP_NAME')}}  {{$title ?? ''}}</title>
+    <title>{{config->app_name}}  {{$title ?? ''}}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="/assets/css/app.css">
+
+    <! -- TODO: Reimplement Inclusion of Screen stylesheets -->
+    @if(isset($screenComponents))
+        <style>
+            @include($screenStylesheet);
+        </style>
+    @endif
 </head> 
 <body>
     
@@ -14,12 +21,20 @@
 
     @reactInitialize
 
+    <script type="module">
+        @yield('imports')
+    </script>
+
     <script type="text/babel">
 
         const { StrictMode } = React;
 
         @include('layouts.components.global');
-        @if(isset($screenComponents)) @include($screenComponents); @endif
+
+        // TODO: Reimplement Inclusion of the Screen Components
+        @if(isset($screenComponents))
+            @include($screenComponents);
+        @endif
 
         const App = () => { return ( <> @yield('jsx') </> ); };
  
